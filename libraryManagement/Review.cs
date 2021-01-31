0f629a36b736b1f19a62d5324ea841e48573c0fe
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-01-30 23:57:29 -0700

    [libraryManagement]--added Review class

---
+++ libraryManagement/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryManagement
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Headline { get; set; }
+        public string ReviewText { get; set; }
+        public int Rating { get; set; }
+
+    }
+}
