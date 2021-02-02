87247170bd9da748c6f2c9d537facbe8e60ac978
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-02 00:21:51 -0700

    [libraryManagement]--Added validation for Caegory and Country Class

--- libraryManagement/models/Category.cs
+++ libraryManagement/models/Category.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace libraryManagement
+namespace libraryManagement.models
 {
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50,ErrorMessage ="Name can be greater than 50 characters")]
         public string Name { get; set; }
+        public virtual ICollection<BookCategory> BookCategories { get; set; }
+
     }
 
 }
--- libraryManagement/models/Country.cs
+++ libraryManagement/models/Country.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace libraryManagement
+namespace libraryManagement.models
 {
     public class Country
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(50,ErrorMessage ="Name can not be more than 50 characters")]
         public int Name { get; set; }
+        //add navigational property
+        public virtual ICollection<Author> Authors {get;set;}  
+
     }
 }
