971ce943c8bbfe0cf2f9432cc029af894ab70b12
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-02 02:31:36 -0700

    [libraryManagement] added DbContext

--- libraryManagement/models/Author.cs
+++ libraryManagement/models/Author.cs
@@ -9,12 +9,13 @@ namespace libraryManagement.models
 {
     public class Author
     {   [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Database generated ID. Since we define ID so we don't need to define but it is for clarification
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] //Database generated ID. Since we define ID so we don't need to define but it is for clarification
         public int Id { get; set; }
         [Required]
-        [MaxLength(100,ErrorMessage message="First name cannot be more than 200 characters")]
+        [MaxLength(100,ErrorMessage ="First name cannot be more than 100 characters")]
         public string FirstName { get; set; }
         [Required]
+        [MaxLength(200, ErrorMessage = "First name cannot be more than 200 characters")]
         public string LastName { get; set; }
         public virtual Country Country { get; set; }
         public virtual ICollection<BookAuthor> BookAuthors { get; set; }
--- libraryManagement/models/BookAuthor.cs
+++ libraryManagement/models/BookAuthor.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace libraryManagement
+namespace libraryManagement.models
 {
     public class BookAuthor
     {
