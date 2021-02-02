00a266299d3b250b645137b28a7063fc83b14334
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-02 01:45:21 -0700

    [libraryManagement]--added database connection String.

--- libraryManagement/Startup.cs
+++ libraryManagement/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +13,25 @@ namespace libraryManagement
 {
     public class Startup
     {
+        //To add the database connection string we need to implement Iconfiguration Interface. It will be static as we will not instantiate it.
+        //This allow us to add connection string in appjson file.
+        public static IConfiguration Configuration { get; set; }
+        //now we like to inject this interface into our startup class (dependency injection). To do that we pass it as argument in constructor
+
+        public Startup (IConfiguration configuration) //passing the interface direct into constructor and value of it can be assign to a property
+        {
+            Configuration = configuration;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+        //we setup the connetionStrings from the configuration property that we set and Iconfiguration interface. Now we define the strings "connectionStrings"
+        //it can be anything
+            var connectionString = Configuration["connectionStrings:libraryDbConnectionStrings"];
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
