1dddeb33473526a349fa61a98bc931b9edc40e51
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-02 00:42:34 -0700

    [libraryManagement]--added Book validation

--- libraryManagement/models/Book.cs
+++ libraryManagement/models/Book.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace libraryManagement
+namespace libraryManagement.models
 {
     public class Book
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [StringLength(10,MinimumLength =3,ErrorMessage ="ISBN must be between 3 and 10 characters")]
         public string Isbn { get; set; }
+        [MaxLength(200,ErrorMessage ="Title length can not be greater thab 200 characters")]
         public string Title { get; set; }
         public DateTime DatePublished { get; set; }
+        public virtual ICollection<Review> reviews { get; set; }
+        public virtual ICollection<BookAuthor> BookAuthors { get; set; }
+        public ICollection<BookCategory> BookCategories { get; set; }
+
     }
 }
