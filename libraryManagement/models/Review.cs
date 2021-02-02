3d9aeaefb9a23a8dec2fde3466a922478ae7e0cf
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-02 00:36:34 -0700

    [libraryManagement]--Added validation for review and reviewer

--- libraryManagement/models/Review.cs
+++ libraryManagement/models/Review.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace libraryManagement
+namespace libraryManagement.models
 {
     public class Review
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [StringLength(200,MinimumLength =10,ErrorMessage ="Headline should be minimum 10 and maximum 200 Characters")]
         public string Headline { get; set; }
+        [Required]
+        [StringLength(2000, MinimumLength = 50, ErrorMessage = "Reviews should be minimum 50 and maximum 2000 Characters")]
         public string ReviewText { get; set; }
         public int Rating { get; set; }
+        public virtual Reviewer Reviewer { get; set; }
+        public virtual Book Book { get; set; }
 
     }
 }
--- libraryManagement/models/Reviewer.cs
+++ libraryManagement/models/Reviewer.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace libraryManagement
+namespace libraryManagement.models
 {
     public class Reviewer
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100,ErrorMessage ="First Name can not be more than 100 characters")]
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(200, ErrorMessage = "First Name can not be more than 200 characters")]
         public string LastName { get; set; }
+
+        //add navigational property
+        public virtual ICollection<Review> Reviews{ get; set; }
 
     }
 }
