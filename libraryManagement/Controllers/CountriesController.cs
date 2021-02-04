7bf1ace8b51475516591a59a3f6d3522d6c4d8f3
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-04 01:47:58 -0700

    [libraryMangement]--Update CountryController

--- libraryManagement/Controllers/CountriesController.cs
+++ libraryManagement/Controllers/CountriesController.cs
@@ -1,4 +1,5 @@
-﻿using libraryManagement.Services;
+﻿using libraryManagement.DTos;
+using libraryManagement.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,10 +20,25 @@ namespace libraryManagement.Controllers
         }
         //api/countries
         [HttpGet]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200,Type=typeof(IEnumerable<CountryDto>))]
         public IActionResult GetCountries()
         {
             var countries = _countryRepository.GetCountries().ToList();
-             return Ok(countries);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            //the countries list will return both country name and null value for author
+            //it is therefore better to use Dto to get the specific field in this case Id and country name
+            var countriesDto = new List<CountryDto>();
+            foreach(var country in countries)
+            {
+                countriesDto.Add(new CountryDto
+                {
+                    Id = country.Id,
+                    Name = country.Name
+                });
+            }
+             return Ok (countriesDto);
         }
     }
 }
