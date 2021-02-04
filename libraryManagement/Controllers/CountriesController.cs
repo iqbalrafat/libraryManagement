6299fc059c1403ad020382c4f51432af417ad631
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-04 00:28:26 -0700

    [libraryManagement]--Created GetCountries API

--- libraryManagement/Controllers/CountriesController.cs
+++ libraryManagement/Controllers/CountriesController.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using libraryManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace libraryManagement.Controllers
 {
-    public class CountriesController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController: Controller
+        
     {
+        private ICountryRepository _countryRepository;
+        public CountriesController(ICountryRepository countryRepository)
+        {
+            _countryRepository = countryRepository;
+        }
+        //api/countries
+        [HttpGet]
+        public IActionResult GetCountries()
+        {
+            var countries = _countryRepository.GetCountries().ToList();
+             return Ok(countries);
+        }
     }
 }
