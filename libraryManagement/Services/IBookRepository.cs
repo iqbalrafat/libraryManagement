7f7c875e0f7ddd44b77c326b0fe904ed79c1d9a2
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-06 10:48:53 -0700

    [libraryManagement]--added BookRepository Interface

---
+++ libraryManagement/Services/IBookRepository.cs
@@ -0,0 +1,19 @@
+﻿using libraryManagement.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryManagement.Services
+{
+    interface IBookRepository
+    {
+        ICollection<Book> GetBooks();
+        Book GetBookById (int bookId);
+        Book GetBookByIsbn(string bookIsbn);        
+        decimal GetBookRating(int bookId);     
+        bool BookExistsById(int bookId);
+        bool BookExistsByIsbn(string bookIsbn);
+        bool IsDuplicateIsbn(int bookId, string bookIsbn);
+    }
+}
