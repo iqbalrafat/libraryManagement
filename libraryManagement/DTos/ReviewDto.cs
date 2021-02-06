e821f74d53a2496d7bbff98c05c8964fc0eec9ea
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-06 09:42:26 -0700

    [libraryManagement]--added Review and Reviewer Dto

---
+++ libraryManagement/DTos/ReviewDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryManagement.DTos
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public string ReviewText { get; set; }
+        public int Rating { get; set; }
+    }
+}
