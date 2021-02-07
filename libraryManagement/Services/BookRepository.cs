9ae5550d933366c57863dda256a5b4ae5eea0bc8
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-07 12:23:42 -0700

    [libraryManagement]--Implemented BookRepository Interface

---
+++ libraryManagement/Services/BookRepository.cs
@@ -0,0 +1,56 @@
+﻿using libraryManagement.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryManagement.Services
+{
+    public class BookRepository:IBookRepository
+    {
+        LibraryDbContext _bookContext;
+        public BookRepository(LibraryDbContext bookContext)
+        {
+            _bookContext = bookContext;
+        }
+
+        public bool BookExistsById(int bookId)
+        {
+            return _bookContext.Books.Any(b => b.Id == bookId);
+        }
+
+        public bool BookExistsByIsbn(string bookIsbn)
+        {
+            return _bookContext.Books.Any(b => b.Isbn==bookIsbn);
+        }
+
+        public Book GetBookById(int bookId)
+        {
+            return _bookContext.Books.Where(b => b.Id == bookId).FirstOrDefault();
+        }
+
+        public Book GetBookByIsbn(string bookIsbn)
+        {
+            return _bookContext.Books.Where(b => b.Isbn==bookIsbn).FirstOrDefault();
+        }
+
+        public decimal GetBookRating(int bookId)
+        {
+            var reviews= _bookContext.Reviews.Where(r => r.Book.Id == bookId);
+            if (reviews.Count() <= 0)
+                return 0;
+            return ((decimal)reviews.Sum(r => r.Rating / reviews.Count()));
+        }
+
+        public ICollection<Book> GetBooks()
+        {
+            return _bookContext.Books.OrderBy(b => b.Title).ToList();
+        }
+
+        public bool IsDuplicateIsbn(int bookId, string bookIsbn)
+        {
+            var book= _bookContext.Books.Where(b => b.Isbn.Trim().ToUpper() == bookIsbn.Trim().ToUpper() && b.Id == bookId);
+            return book == null ? false : true;
+        }
+    }
+}
