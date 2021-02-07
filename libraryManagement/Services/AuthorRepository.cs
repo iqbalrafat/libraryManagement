940a81cf0503879b7e95cbbafb878b61b9a30640
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-07 11:05:24 -0700

    [libraryManagement]--implemented AuthorRepository Interface

---
+++ libraryManagement/Services/AuthorRepository.cs
@@ -0,0 +1,41 @@
+﻿using libraryManagement.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryManagement.Services
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private LibraryDbContext _authorContext;
+        public AuthorRepository(LibraryDbContext authorContext)
+        {
+            _authorContext = authorContext;
+        }
+        public bool AuthorExists(int authorId)
+        {
+            return _authorContext.Authors.Any(a => a.Id == authorId);
+        }
+
+        public Author GetAuthor(int authorId)
+        {
+            return _authorContext.Authors.Where(a => a.Id == authorId).FirstOrDefault();
+        }
+
+        public ICollection<Author> GetAuthors()
+        {
+            return _authorContext.Authors.OrderBy(a => a.FirstName).ToList();
+        }
+
+        public ICollection<Author> GetAuthorsOfABook(int bookId)
+        {
+            return _authorContext.BookAuthors.Where(b => b.BookId == bookId).Select(a =>a.Author).ToList();
+        }
+
+        public ICollection<Book> GetBooksByAuthor(int authorId)
+        {
+            return _authorContext.BookAuthors.Where(a => a.AuthorId == authorId).Select(b => b.Book).ToList();
+        }
+    }
+}
--- libraryManagement/Startup.cs
+++ libraryManagement/Startup.cs
@@ -42,8 +42,6 @@ namespace libraryManagement
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IReviewerRepository, ReviewerRepository>();
             services.AddScoped<IReviewRepository, ReviewRepository>();
-
-
         }       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
