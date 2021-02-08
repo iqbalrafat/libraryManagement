9fe8507dca8591b1936f207125e3c20645fc009f
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-08 10:33:10 -0700

    [libraryManagement]--Added GetBookById API

--- libraryManagement/Controllers/BooksController.cs
+++ libraryManagement/Controllers/BooksController.cs
@@ -37,45 +37,47 @@ namespace libraryManagement.Controllers
                     DatePublished=book.DatePublished
                 });
             }
-
             return Ok(booksDto);
         }
+        //api/bOOKS/{bookId}
+        [HttpGet("{bookId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(BookDto))]
+        public IActionResult GetBookById(int bookId)
+        {
+            if (!_bookRepository.BookExistsById(bookId))
+                return NotFound();
+            var book = _bookRepository.GetBookById(bookId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var bookDto = new BookDto()
+            {
+                    Id = book.Id,
+                    Title = book.Title,
+                    Isbn = book.Isbn,
+                    DatePublished = book.DatePublished
+             };
+            
+            return Ok(bookDto);
+        }
+        //api/books/{bookId}/rating
+        [HttpGet("{bookId}/rating")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        public IActionResult GetBookRating(int bookId)
+        {
+            if (!_bookRepository.BookExistsById(bookId))
+                return NotFound();
+            var rating = _bookRepository.GetBookRating(bookId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+           return Ok(rating);
+        }
+
 
     }
 }
 
 
-//public bool BookExistsById(int bookId)
-//{
-//    return _bookContext.Books.Any(b => b.Id == bookId);
-//}
-
-//public bool BookExistsByIsbn(string bookIsbn)
-//{
-//    return _bookContext.Books.Any(b => b.Isbn == bookIsbn);
-//}
-
-//public Book GetBookById(int bookId)
-//{
-//    return _bookContext.Books.Where(b => b.Id == bookId).FirstOrDefault();
-//}
-
-//public Book GetBookByIsbn(string bookIsbn)
-//{
-//    return _bookContext.Books.Where(b => b.Isbn == bookIsbn).FirstOrDefault();
-//}
-
-//public decimal GetBookRating(int bookId)
-//{
-//    var reviews = _bookContext.Reviews.Where(r => r.Book.Id == bookId);
-//    if (reviews.Count() <= 0)
-//        return 0;
-//    return ((decimal)reviews.Sum(r => r.Rating / reviews.Count()));
-//}
-
-
-//public bool IsDuplicateIsbn(int bookId, string bookIsbn)
-//{
-//    var book = _bookContext.Books.Where(b => b.Isbn.Trim().ToUpper() == bookIsbn.Trim().ToUpper() && b.Id == bookId);
-//    return book == null ? false : true;
-//}
