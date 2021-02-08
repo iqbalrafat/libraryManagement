15bb30dd1497544ff1192695707c734a422588c3
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-07 23:23:43 -0700

    [libraryManagement] -- added GetReviews API

---
+++ libraryManagement/Controllers/ReviewsController.cs
@@ -0,0 +1,41 @@
+﻿using libraryManagement.DTos;
+using libraryManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryManagement.Controllers
+{
+    [Route("api/[Controller]")]
+    public class ReviewsController: Controller
+    {
+        private IReviewRepository _reviewRepository;
+        public ReviewsController(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+        //api/reviews
+        [HttpGet]
+        [ProducesResponseType(200, Type =typeof(IEnumerable<ReviewDto>))]
+        public IActionResult GetReviews()
+        {
+            var Reviews = _reviewRepository.GetReviews();
+            if (!ModelState.IsValid)
+                BadRequest(ModelState);
+            var reviewsDto = new List<ReviewDto>();
+            foreach(var review in Reviews)
+            {
+                reviewsDto.Add(new ReviewDto
+                {
+                    Id = review.Id,
+                    Headline=review.Headline,
+                    ReviewText=review.ReviewText,
+                    Rating=review.Rating,
+                });
+            }
+            return Ok(reviewsDto);
+        }
+    }
+}
--- libraryManagement/Startup.cs
+++ libraryManagement/Startup.cs
@@ -42,6 +42,8 @@ namespace libraryManagement
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IReviewerRepository, ReviewerRepository>();
             services.AddScoped<IReviewRepository, ReviewRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddScoped<IAuthorRepository, AuthorRepository>();
         }       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
