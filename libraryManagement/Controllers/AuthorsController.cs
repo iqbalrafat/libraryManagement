6f564286d8eb26537d500382c634b7849af4c1e5
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-08 02:10:23 -0700

    [libraryMangement]--Added GetAuthor API

---
+++ libraryManagement/Controllers/AuthorsController.cs
@@ -0,0 +1,94 @@
+﻿using libraryManagement.DTos;
+using libraryManagement.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace libraryManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController:Controller
+    {
+        private IAuthorRepository _authorRepository;
+        private IBookRepository _bookRepository;
+
+        public AuthorsController(IAuthorRepository authorRepository, IBookRepository bookRepository)
+        {
+            _authorRepository = authorRepository;
+            _bookRepository = bookRepository;
+        }
+        //api/Authors
+        [HttpGet]
+        [ProducesResponseType(200,Type =typeof(IEnumerable<AuthorDto>))]
+        public IActionResult GetAuthors()
+        {
+            var authors = _authorRepository.GetAuthors();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+           var  authorsDto = new List<AuthorDto>();
+            foreach(var author in authors)
+            {
+                authorsDto.Add(new AuthorDto
+                {
+                    id = author.Id,
+                    FirstName = author.FirstName,
+                    LastName = author.LastName
+                });
+            }
+            return Ok (authorsDto);
+        }
+        //api/Authors/{authorId}
+        [HttpGet("{authorId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(AuthorDto))]
+        public IActionResult GetAuthor(int authorId)
+        {
+            if (!_authorRepository.AuthorExists(authorId))
+                return NotFound();
+            var author = _authorRepository.GetAuthor(authorId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var authorDto = new AuthorDto()
+            {
+                id = author.Id,
+                FirstName = author.FirstName,
+                LastName = author.LastName
+            };
+            return Ok(authorDto);
+        }
+        //api/Authors/books/{bookId}
+        [HttpGet("books/{bookId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<AuthorDto>))]
+        public IActionResult GetAuthorsOfABook(int bookId)
+        {
+            if (!_bookRepository.BookExistsById(bookId))
+                return NotFound();
+            var authors = _authorRepository.GetAuthorsOfABook(bookId);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var authorsDto = new List<AuthorDto>();
+            foreach (var author in authors)
+            {
+                authorsDto.Add(new AuthorDto
+                {
+                    id = author.Id,
+                    FirstName = author.FirstName,
+                    LastName = author.LastName
+                });
+            }
+            return Ok(authorsDto);
+        }
+
+
+
+
+
+
+     }
+}
