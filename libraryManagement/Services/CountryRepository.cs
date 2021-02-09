ae6bd62ddcbd1918e4f73d17220c2e8efaae3a4f
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-08 23:46:05 -0700

    [libraryManagement]--update CountryRepository with Update Method

--- libraryManagement/Services/CountryRepository.cs
+++ libraryManagement/Services/CountryRepository.cs
@@ -18,6 +18,16 @@ namespace libraryManagement.Services
             return _countryContext.Countries.Any(c => c.Id == countryId);
         }
 
+        public bool CreateCountry(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCountry(Country country)
+        {
+            throw new NotImplementedException();
+        }
+
         public ICollection<Author> GetAuthorsFromACountry(int countryId)
         {
             return _countryContext.Authors.Where(c => c.Country.Id == countryId).ToList();
@@ -37,6 +47,26 @@ namespace libraryManagement.Services
         public Country GetCountryOfAnAuthor(int authorId)
         {
             return _countryContext.Authors.Where(a => a.Id == authorId).Select(c => c.Country).FirstOrDefault();
+        }
+
+        public bool IsDuplicateCountryName(int countryId, string countryName)
+        {
+            var country =_countryContext.Countries.Where(c => c.Name.Trim().ToUpper() == countryName.Trim().ToUpper() && c.Id == countryId).FirstOrDefault();
+            return country == null ? false : true;
+        }
+
+        public bool Save()
+        {
+      //to check the CRUD Operation successeded we use SaveChanges() method. It returns 0, 1 or -ve number. if it 
+      //is zero mean nothing but not error. If it is 1 mean changed occur successfully, -ve mean an error
+      //we use this method for all our crud operation.
+            var saved = _countryContext.SaveChanges();
+            return saved >=0 ? true : false;
+        }
+        public bool UpdateCountry(Country country)
+        {
+            _countryContext.Update(country);
+            return Save();
         }
     }
 }
