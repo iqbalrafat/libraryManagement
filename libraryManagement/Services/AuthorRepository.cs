401395417af268be76a3f944cb3d831bd3e7870d
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-11 13:12:40 -0700

    [libraryMangement]--implemented CRUD method to AuthorRepository

--- libraryManagement/Services/AuthorRepository.cs
+++ libraryManagement/Services/AuthorRepository.cs
@@ -18,6 +18,18 @@ namespace libraryManagement.Services
             return _authorContext.Authors.Any(a => a.Id == authorId);
         }
 
+        public bool CreateAuthor(Author author)
+        {
+            _authorContext.Add(author);
+            return Save();
+        }
+
+        public bool DeleteAuthor(Author author)
+        {
+            _authorContext.Remove(author);
+            return Save();
+        }
+
         public Author GetAuthor(int authorId)
         {
             return _authorContext.Authors.Where(a => a.Id == authorId).FirstOrDefault();
@@ -36,6 +48,18 @@ namespace libraryManagement.Services
         public ICollection<Book> GetBooksByAuthor(int authorId)
         {
             return _authorContext.BookAuthors.Where(a => a.AuthorId == authorId).Select(b => b.Book).ToList();
+        }
+
+        public bool Save()
+        {
+            var saved = _authorContext.SaveChanges();
+            return saved >= 0 ? true : false;
+        }
+
+        public bool UpdateAuthor(Author author)
+        {
+            _authorContext.Update(author);
+            return Save();
         }
     }
 }
