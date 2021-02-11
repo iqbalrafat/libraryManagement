145403f0880c46787b3a0af77a52cd0f0652f3fc
iqbalrafat <64336748+iqbalrafat@users.noreply.github.com>
2021-02-11 12:36:27 -0700

    [libraryMangement]--Implemented CRUD method to ReviewRepository

--- libraryManagement/Services/ReviewRepository.cs
+++ libraryManagement/Services/ReviewRepository.cs
@@ -13,6 +13,19 @@ namespace libraryManagement.Services
         {
             _reviewContext = reviewContext;
         }
+
+        public bool CreateReview(Review review)
+        {
+            _reviewContext.Add(review);
+            return Save();
+        }
+
+        public bool DeleteReview(Review review)
+        {
+            _reviewContext.Remove(review);
+            return Save();
+        }
+
         public Book GetBookOfAReview(int reviewId)
         {
             var bookId = _reviewContext.Reviews.Where(r => r.Id == reviewId).Select(r => r.Book.Id).FirstOrDefault();
@@ -37,6 +50,18 @@ namespace libraryManagement.Services
         public bool ReviewExists(int reviewId)
         {
             return _reviewContext.Reviews.Any(r => r.Id == reviewId);
+        }
+
+        public bool Save()
+        {
+            var saved = _reviewContext.SaveChanges();
+            return saved >= 0 ? true : false;
+        }
+
+        public bool UpdateReview(Review review)
+        {
+            _reviewContext.Update(review);
+            return Save();
         }
     }
 }
